95c0fbb5813e69cff8a7247a1828f9c38208a394
tienbob <tienpop5@gmail.com>
2026-03-24 11:29:38 +0700

    modify user model change devise mail setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_one_attached :avatar
-
   enum role: { author: 0, admin: 1 }
-
   validates :name, presence: true
-
-  private
-
-  def company_email_domain
-    allowed = ENV.fetch("ALLOWED_EMAIL_DOMAIN", "company.com").downcase
-    unless email.to_s.downcase.end_with?("@#{allowed}")
-      errors.add(:email, "must be a company email (@#{allowed})")
-    end
-  end
+  
 end
