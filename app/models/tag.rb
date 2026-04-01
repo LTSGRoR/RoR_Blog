81382e8ea7d71001d42f32a782249b76ad080ff5
tienbob <tienpop5@gmail.com>
2026-04-01 10:04:20 +0700

    backend logic improve

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,14 @@
 class Tag < ApplicationRecord
+  searchkick word_start: [:name]
+
   has_many :taggings, dependent: :destroy
   has_many :posts, through: :taggings
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  def search_data
+    { name: name }
+  end
 
   before_save :normalize_name
 
