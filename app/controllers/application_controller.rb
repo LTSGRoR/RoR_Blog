d91818e59b55665a05f6879f40d5b8ea744fdf5f
tienbob <tienpop5@gmail.com>
2026-04-06 10:17:11 +0700

    implement i18n for three language Eng, VN, JP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,23 @@ class ApplicationController < ActionController::Base
   # Rescue from authorization errors and show a friendly message.
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   private
 
+  def set_locale
+    locale = params[:locale]&.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
+  end
+
   def user_not_authorized(exception)
     policy_name = exception.policy.class.to_s.underscore
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = t("errors.not_authorized")
     redirect_to(request.referer || root_path)
   end
 
