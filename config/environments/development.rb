19ee1cac09b018ac7faa521122b518c35c8a419b
tienbob <tienpop5@gmail.com>
2026-04-14 10:24:21 +0700

    fix sidekiq docker build

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  # Use Letter Opener in development to preview emails in the browser.
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :file
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.file_settings = { location: Rails.root.join("tmp/mails") }
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
