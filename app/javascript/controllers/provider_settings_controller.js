05e25af63c3747e4fff35da2e27aae9fc660d3ac
tienbob <tienpop5@gmail.com>
2026-06-01 11:20:37 +0700

    hotfix ai setting form

--- app/javascript/controllers/provider_settings_controller.js
+++ app/javascript/controllers/provider_settings_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["providerSelect", "apiKeyWrapper", "apiKeyInput"]
+  static values = { storedApiKey: Boolean }
 
   connect() {
     this.toggleApiKey()
@@ -16,11 +17,12 @@ export default class extends Controller {
 
     const provider = this.providerSelectTarget.value
     const requiresApiKey = provider !== "ollama"
+    const shouldRequireApiKey = requiresApiKey && !this.storedApiKeyValue
 
     this.apiKeyWrapperTarget.classList.toggle("hidden", !requiresApiKey)
 
     if (this.hasApiKeyInputTarget) {
-      this.apiKeyInputTarget.required = requiresApiKey
+      this.apiKeyInputTarget.required = shouldRequireApiKey
     }
   }
 }
